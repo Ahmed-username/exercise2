587c0d533cb1b74273851276c52f615851de4767
Ahmed <dulaimi89@gmail.com>
2018-09-05 04:31:35 +0100

    grabbed current week

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+var calendarJS = require("calendar-js")
 
 class App extends React.Component {
   constructor(){
@@ -7,13 +8,29 @@ class App extends React.Component {
   }
 
   componentDidMount(){
-    fetch(`https://assessments.bzzhr.net/calendar/?format=json`)
-    .then(response=>response.json())
-    .then(json=>console.log(json.results))
-    .catch(err=>console.log(err))
+
+
+    // fetch(`https://assessments.bzzhr.net/calendar/?format=json`)
+    // .then(response=>response.json())
+    // .then(json=>console.log(json.results))
+    // .catch(err=>console.log(err))
   }
 
   render(){
+    let currentDate = new Date();
+    // console.log(" date",currentDate)
+     console.log("getDay",currentDate.getDay())
+    // console.log("getDate",currentDate.getDate())
+    let myCalendar= calendarJS().of(currentDate.getFullYear(),currentDate.getMonth()).calendar;
+    console.log(myCalendar)
+    let currentWeek=[];
+    myCalendar.forEach(week=>{
+     if(week.indexOf(currentDate.getDate()) >-1)
+      currentWeek=week;
+    })
+    console.log("current week", currentWeek)
+   
+
     return (
       <div>
         React App
